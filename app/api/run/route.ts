bef6ee92bcdfc759e4ba12e3bae8aa7805114505
Deepa <deepaksutradhar415@gmail.com>
2026-02-13 16:03:19 +0530

    post

---
+++ app/api/run/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { runWorkflow, StepType } from "@/lib/engine";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req : Request) {
+    try{
+        const {text, steps} = await req.json();
+    
+        if(!text || !steps || steps.length == 0){
+            return NextResponse.json({
+                message : "Input text or steps missing",
+                status : 400,
+            });
+        }
+
+        let currentInput = text;
+        const results = []
+
+        for(const step of steps){
+            const output = await runWorkflow(step as StepType, text);
+            results.push({step, output});
+            currentInput = text;
+        }
+
+        await prisma.workflow.create({
+            data : {
+                inputText : currentInput,
+                results : results,
+            }
+        });
+
+        return NextResponse.json({results});
+    }catch(error:any){
+        console.error(error);
+        return NextResponse.json({
+            message : "Internal server error, unable to post", 
+            details : error.message,
+            status : 500,
+        });
+    }
+}
