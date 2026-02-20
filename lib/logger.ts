5678c7138cf202f863d1a6cf58be256e13b68d6e
Deepa <deepaksutradhar415@gmail.com>
2026-02-20 22:51:10 +0530

    pino logger

---
+++ lib/logger.ts
@@ -0,0 +1,18 @@
+import pino from "pino";
+
+const isProd = process.env.NODE_ENV === "production";
+
+export const logger = pino({
+    level : process.env.LOG_LEVEL,
+    base : {
+        service : "workflow-builder-lite",
+    },
+    transport : !isProd  ? {
+        target : "pino-pretty",
+        options : {
+            colorize : true,
+            translateTime : "SYS:standard",
+            ignore : "pid,hostname",
+        }
+    } : undefined,
+});
