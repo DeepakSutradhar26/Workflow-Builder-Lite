bf28ace4cc288909fe4757dab25bc7f59cfef77d
Deepa <deepaksutradhar415@gmail.com>
2026-02-21 19:44:36 +0530

    bug fix logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,18 +1,13 @@
 import pino from "pino";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export const logger = pino({
-    level : process.env.LOG_LEVEL,
-    base : {
-        service : "workflow-builder-lite",
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      translateTime: "HH:MM:ss",
+      ignore: "pid,hostname",
     },
-    transport : !isProd  ? {
-        target : "pino-pretty",
-        options : {
-            colorize : true,
-            translateTime : "SYS:standard",
-            ignore : "pid,hostname",
-        }
-    } : undefined,
+  },
 });
